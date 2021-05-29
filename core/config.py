f51eb0b2fbb8c2a8606079cd2b5fb816d645a283
ravishankarramakrishnan <ravish7895@gmail.com>
2021-05-29 14:45:27 +0530

    Connection to DB V2

--- core/config.py
+++ core/config.py
@@ -1,8 +1,27 @@
 # Here is where all configurations are maintained for this Application
+
+# Import Environment Variables in Config.py file
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Environment Path - Current Folder Check
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+
 class Settings:
     """We are going to keep track of Project in this Class"""
     PROJECT_TITLE: str = "Jobboard"
     PROJECT_VERSION: str = "0.1.0"
-    
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)  # Default Postgres PORT
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "db_jobboard")
+    # Create a Database URL
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
 # Create Object of Class
 settings = Settings()
--- main.py
+++ main.py
@@ -3,11 +3,29 @@
 # Importing Libraries
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
 
+# Define a Function to Create Tables
+def create_tables():
+    # The below code is used to connect FastAPI to Database Engine
+    # DB will be Created everytime we start a process (Database will stay intact)
+    Base.metadata.create_all(bind=engine)
 
-# Initialize FastAPI
-app = FastAPI(title= settings.PROJECT_TITLE, version= settings.PROJECT_VERSION)
+
+# Define a Function to Start the Application
+def start_application():
+    # Initialize FastAPI
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    # Create Tables
+    create_tables()
+    # Return App
+    return app
+
+
+# Define App
+app = start_application()
 
 
 # Create Routes
